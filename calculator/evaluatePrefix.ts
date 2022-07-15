d6462268a487b2884cfc7c1d1bcf1f543f25630d
mahrokh <mahrokh.nz@gmail.com>
2022-07-15 11:04:40 +0430

    feat: add types

--- calculator/evaluatePrefix.ts
+++ calculator/evaluatePrefix.ts
@@ -8,7 +8,7 @@ class EvaluatePrefix {
   }
 
   //should move to a new class
-  isOperator(charX: string) {
+  isOperator(charX: string): boolean {
     switch (charX) {
       case "+":
       case "-":
@@ -20,20 +20,20 @@ class EvaluatePrefix {
   }
 
   createStackArray() {
-    const plate = []; //keeping
-    const stackArray = new Stack(); //stack pre to po
+    const plate: (string | number)[] = []; //keeping
+    const stackArray: Stack = new Stack(); //stack pre to po
     for (let i = this.string.length - 1; i >= 0; i--) {
       if (this.isOperator(this.string[i])) {
-        const s1 = stackArray.pop();
+        const s1: number = stackArray.pop();
         plate.push(s1);
 
-        const s2 = stackArray.pop();
+        const s2: number = stackArray.pop();
         plate.push(s2);
 
         plate.push(this.string[i]);
 
-        const plateCalculator = `${plate[0]} ${plate[2]} ${plate[1]}`; //calculating
-        const result = eval(plateCalculator);
+        const plateCalculator: string = `${plate[0]} ${plate[2]} ${plate[1]}`; //calculating
+        const result: number = eval(plateCalculator);
 
         stackArray.push(result);
       } else {
