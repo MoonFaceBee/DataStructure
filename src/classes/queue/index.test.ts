f7ff7448fd419fd57b1bee8a125530acc2c75f30
mahrokh <mahrokh.nz@gmail.com>
2022-07-18 12:54:51 +0430

    feat: add test file

---
+++ src/classes/queue/index.test.ts
@@ -0,0 +1,44 @@
+import Queue from "./index.ts";
+import {assertEquals} from "https://deno.land/std@0.148.0/testing/asserts.ts";
+
+Deno.test("Queue", () => {
+	const q = new Queue(3)
+
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	q.enqueue(4)
+
+	assertEquals(q.isFull, true)
+
+	assertEquals(q.peek(), 1)
+
+	q.dequeue()
+
+	assertEquals(q.peek(), 2)
+
+	q.dequeue()
+	q.dequeue()
+
+	assertEquals(q.isEmpty, true)
+})
+
+Deno.test('Contains', () => {
+	const q = new Queue(2)
+
+	q.enqueue(1)
+	q.enqueue(2)
+
+	assertEquals(q.contains(2), true)
+})
+
+Deno.test('Reset', () => {
+	const q = new Queue(2)
+
+	q.enqueue(1)
+	q.enqueue(2)
+
+	q.empty()
+
+	assertEquals(q.isEmpty, true)
+})
