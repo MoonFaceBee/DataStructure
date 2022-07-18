5d4ca7ac55a3179249d05509cf3b9ad8f22f4f78
mahrokh <mahrokh.nz@gmail.com>
2022-07-18 12:56:49 +0430

    fix: types

--- src/classes/stack/index.ts
+++ src/classes/stack/index.ts
@@ -1,42 +1,43 @@
-//  TODO: Use <Generics>
+export type TStackType = string | number | undefined;
+
 class Stack {
-  //Array, Length, Last Index(top)
-  private array: any[] = [];
+  //Array, Length
+  private array: Array<TStackType> = [];
+
+  get length(): number {
+    return this.array.length;
+  }
 
   //Check if the element is empty
   get isEmpty(): boolean {
     return this.array.length === 0;
   }
 
-  get length(): number {
-    return this.array.length;
-  }
-
   //Push to array
-  push(newItem: any): any {
+  push(newItem: TStackType): this {
     this.array[this.array.length] = newItem;
 
     return this;
   }
 
   //Pop element
-  pop(): any {
+  pop(): TStackType {
     const val = this.array.at(-1);
     this.array.length -= 1;
-    return val;
+    return val as TStackType;
   }
 
   //Get the value of last element
-  peek(): any {
-    return this.array.at(-1);
+  peek(): TStackType {
+    return this.array.at(-1) as TStackType;
   }
 
   //Empty the array
-  empty(): any {
+  empty() {
     this.array.length = 0;
   }
 
-  contains(item: any): boolean {
+  contains(item: TStackType): boolean {
     return this.array.includes(item);
   }
 }
