b99cbed660a8a42fa13f68ac022fadfa28c9d939
Mahrokh Nabizadeh <mahrokh.nz@gmail.com>
2022-08-15 16:55:26 +0430

    feat: add insertion

--- src/classes/binary_search_tree/index.ts
+++ src/classes/binary_search_tree/index.ts
@@ -1,22 +1,49 @@
 import BinaryTree, {BinaryTreeNode} from "../binary_tree/index.ts";
 
 class BinarySearchTree<T> extends BinaryTree<T> {
-	constructor(rootData: T) {
-		super(rootData);
+	constructor() {
+		super();
 	}
 
-	private insertNodeHelper(node: BinaryTreeNode<T>) {
-		if (node < node.tree.root) {
-			node.tree.root.left.setLeft(node)
+	private insertNodeHelper(data: T, node: BinaryTreeNode<T>) {
+		if (data <= node.data) {
+			if (node.left) {
+				this.insertNodeHelper(data, node.left)
+			} else {
+				node.setLeft(data);
+			}
+		} else {
+			if (node.right) {
+				this.insertNodeHelper(data, node.right)
+			} else {
+				node.setRight(data)
+			}
 		}
 	}
 
-	insertNode(node: BinaryTreeNode<T>): void {
-		this.insertNodeHelper(node)
+	insert(data: T): void {
+		if (this.root === null) {
+			this.root = new BinaryTreeNode<T>(this, data);
+		} else {
+			this.insertNodeHelper(data, this.root)
+		}
 	}
 }
 
 export default BinarySearchTree
 
-const bst = new BinarySearchTree(1)
-bst.insertNode(5)
+const bst = new BinarySearchTree<number>();
+bst.insert(8)
+bst.insert(2)
+bst.insert(10)
+bst.insert(5)
+bst.insert(1)
+
+console.log(bst.root.data)
+console.log(bst.root.left.data)
+console.log(bst.root.right.data)
+console.log(bst.root.left.right.data)
+console.log(bst.root.left.left.data)
+
+
+bst.print();
