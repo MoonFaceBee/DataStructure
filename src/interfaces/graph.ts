91f5b6f557f1bcf49e0ff9fde95b29e7a8293138
Mahrokh Nabizadeh <mahrokh.nz@gmail.com>
2022-08-21 12:22:04 +0430

    feat: add graph interface

--- src/interfaces/graph.ts
+++ src/interfaces/graph.ts
@@ -1,5 +1,17 @@
-interface IGraph<T> {
-	addVertex(value: T): void;
+import { GraphNode } from "../classes/graph/index.ts";
+import LinkedList from "../classes/linked_list/index.ts";
+
+export interface IGraphNode<T> {
+  link(node: GraphNode<T>, backlink?: boolean): void;
+  unlink(node: GraphNode<T>, backlink?: boolean): void;
+
+	linksTo(node: GraphNode<T>): boolean;
 }
 
-export default IGraph;
+export interface IGraph<T> {
+  add(node: GraphNode<T>): void;
+  remove(node: GraphNode<T>, unlink?: boolean): void;
+
+  adjacencyMatrix(): number[][];
+  adjacencyList(): LinkedList<number>[];
+}
