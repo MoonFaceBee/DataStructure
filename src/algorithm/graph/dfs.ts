b7430244213551c5d6705dff3c7045f0dd3007ba
Mahrokh Nabizadeh <mahrokh.nz@gmail.com>
2022-08-22 15:41:20 +0430

    feat: add dfs and test

---
+++ src/algorithm/graph/dfs.ts
@@ -0,0 +1,36 @@
+import {GraphNode} from "../../classes/graph/index.ts";
+
+function dfs<T>(start: GraphNode<T>, goal: GraphNode<T>): GraphNode<T>[] {
+	if (start === goal) {
+		return [start]
+	}
+
+	if (start.links.length === 0) {
+		return []
+	}
+
+	let visited: GraphNode<T>[] = []
+	let stack: GraphNode<T>[] = []
+
+	visited.push(start)
+	stack.push(...start.links);
+
+	while (stack.length > 0) {
+		const node = stack.pop() as GraphNode<T>
+		visited.push(node)
+		
+		if (node === goal) {
+			return visited
+		}
+
+		const unvisitedNodes = node.links.filter((n) => !visited.includes(n))
+		
+		if (unvisitedNodes.length > 0) {
+			stack.push(...unvisitedNodes)
+		}
+	}
+
+	return []
+}
+
+export default dfs;
